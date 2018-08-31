fb87301e63a7b8894b750eb809eee04074e072fe
Andy Park <andy@bigbearlabs.com>
2018-08-31 14:28:08 +0200

    reimplement diff-based table view refreshing using FlexibleDiff since its API is cleaner.

--- ListOrganisedView/GenericCollectionViewController.swift
+++ ListOrganisedView/GenericCollectionViewController.swift
@@ -1,80 +1,63 @@
 import Cocoa
-import DifferenceKit
-
-
-
-//extension GenericCollectionItemModel where Self: Differentiable {
-//
-//}
-
-struct DiffableElement: Differentiable {
-  var differenceIdentifier: String {
-    return model.id
-  }
-  
-  func isContentEqual(to source: DiffableElement) -> Bool {
-    return "\(source.model.dictionaryRepresentation)" == "\(self.model.dictionaryRepresentation)"
-  }
-  
-  typealias DifferenceIdentifier = String
-  
-  let model: GenericCollectionItemModel
-}
-
+import FlexibleDiff
 
 
 public protocol GenericCollectionItemModel {
   var id: String { get }
   var title: String? { get }
   var tooltipText: String? { get }
-  
+
   var dictionaryRepresentation: [String : Any?] { get }
 }
 
-extension ElementPath {
-  var indexPath: IndexPath {
-    return IndexPath(item: self.element, section: self.section)
-  }
-}
 
 public class GenericCollectionViewController: NSViewController {
-  
-  
+
   public var itemModels: [GenericCollectionItemModel] = [] {
     didSet {
       (collectionView?.dataSource as? GenericCollectionViewDataSource)?.itemModels = self.itemModels
-      
+
       if oldValue.isEmpty {
         // first load
         collectionView?.reloadData()
         return
       }
-      
-      let (old, new) = (
-        oldValue.map { DiffableElement(model: $0) },
-        itemModels.map { DiffableElement(model: $0) }
-      )
-      
-      let changeSet = StagedChangeset(source: old, target: new)
-      
-      for changes in changeSet {
 
-        collectionView?.deleteItems(
-          at: Set(changes.elementDeleted.map { $0.indexPath }))
-        
-        collectionView?.insertItems(
-          at: Set(changes.elementInserted.map { $0.indexPath }))
-        
-        collectionView?.reloadItems(
-          at: Set(changes.elementUpdated.map { $0.indexPath }))
-        
-        for (source, target) in changes.elementMoved {
-          collectionView?.moveItem( at: source.indexPath, to: target.indexPath)
-        }
-      }
+      refreshCollectionViewWithData(from: oldValue, to: self.itemModels)
     }
   }
-  
+
+  func refreshCollectionViewWithData(from: [GenericCollectionItemModel], to: [GenericCollectionItemModel]) {
+
+    let changeset = Changeset(
+      previous: from,
+      current: to,
+      identifier: { model in model.id },
+      areEqual: { a, b in
+        return "\(a.dictionaryRepresentation)" == "\(b.dictionaryRepresentation)"
+      })
+    
+    collectionView?.performBatchUpdates({
+      collectionView?.insertItems(
+        at: Set(changeset.inserts.map { IndexPath(item: $0, section: 0) }))
+      
+      collectionView?.deleteItems(
+        at: Set(changeset.removals.map { IndexPath(item: $0, section: 0) }))
+      
+      for move in changeset.moves {
+        collectionView?.moveItem( at: IndexPath(item: move.source, section: 0), to: IndexPath(item: move.destination, section: 0))
+      }
+      
+      collectionView?.reloadItems(
+        at: Set(changeset.mutations.map { IndexPath(item: $0, section: 0) }))
+
+    }, completionHandler: { finished in
+    })
+    
+
+  }
+
+
   public var collectionItemNib: NSNib? {
     didSet {
       self.collectionView?.register(
@@ -83,36 +66,36 @@ public class GenericCollectionViewController: NSViewController {
       )
     }
   }
-  
+
   public var onSelect: ((_ modelObjects: [GenericCollectionItemModel], _ vc: GenericCollectionViewController) -> ())?
 
   public var onDoubleClick: ((_ modelObject: GenericCollectionItemModel, _ vc: GenericCollectionViewController) -> ())?
 
   public var itemHeight: CGFloat = 80  // reasonable default.
-  
-  
-  
+
+
+
   public var selectedItemModels: [GenericCollectionItemModel] {
     let indexes = self.collectionView!.selectionIndexPaths
     let models = indexes.map {
       (self.collectionView!.dataSource as! GenericCollectionViewDataSource).viewModel(indexPath: $0)
     }
-    
+
     return models
   }
-  
-  
+
+
   @IBOutlet
   weak var collectionView: NSCollectionView?
-  
+
   @IBOutlet
   var collectionViewDataSource: GenericCollectionViewDataSource?
-  
+
   override public func viewWillAppear() {
     super.viewWillAppear()
-    
+
     // trigger the didSets.
-    
+
     let items = self.itemModels
     self.itemModels = items
 
@@ -123,24 +106,24 @@ public class GenericCollectionViewController: NSViewController {
   @IBAction func action_addListItem(_ sender: Any) {
     // present 'add item' scene.
   }
-  
+
 
   @IBAction func action_editListItem(_ sender: Any) {
     // present 'edit item' scene.
   }
-  
-  
+
+
   // confirm add
-  
+
   // confirm edit
-  
+
   // confirm delete
-  
+
   // cancel
-  
-  
+
+
   // further down the line: re-order
-  
+
   // further down the line: drag-drop
 }
 
@@ -148,12 +131,12 @@ public class GenericCollectionViewController: NSViewController {
 // MARK: - delegate
 
 extension GenericCollectionViewController: NSCollectionViewDelegate {
-  
+
   public func collectionView(_ collectionView: NSCollectionView, didSelectItemsAt indexPaths: Set<IndexPath>) {
     let modelObjects = self.selectedItemModels
     self.onSelect?(modelObjects, self)
   }
-  
+
   public func collectionView(_ collectionView: NSCollectionView, didDeselectItemsAt indexPaths: Set<IndexPath>) {
     let modelObjects = self.selectedItemModels
     self.onSelect?(modelObjects, self)
@@ -169,41 +152,42 @@ extension GenericCollectionViewController: NSCollectionViewDelegateFlowLayout {
 
 
 class GenericCollectionViewDataSource: NSObject, NSCollectionViewDataSource {
-  
+
   var itemModels: [GenericCollectionItemModel]!
-  
+
   func viewModel(indexPath: IndexPath) -> GenericCollectionItemModel {
     return itemModels[indexPath.item]
   }
-  
-  
+
+
   // MARK: - NSCollectionViewDataSource
-  
+
   func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
 
     let viewModel = self.viewModel(indexPath: indexPath)
-    
+
     // precondition: nib registered with same item id.
     let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "DefaultCollectionViewItem"), for: indexPath)
     item.representedObject = viewModel.dictionaryRepresentation
-    
+
     item.view.onDoubleClick = item.view.onDoubleClick
       ?? {
       let vc = collectionView.delegate as! GenericCollectionViewController
       vc.onDoubleClick!(viewModel, vc)
     }
-    
+
     return item
   }
-  
-  
+
+
   func numberOfSections(in collectionView: NSCollectionView) -> Int {
     // no sections for now.
     return 1
   }
-  
+
   func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
     return itemModels?.count ?? 0
   }
-  
+
 }
+
